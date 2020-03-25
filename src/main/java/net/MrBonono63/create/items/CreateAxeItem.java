537338336f384aaea19f8d600682c139d93359a1
Bonono63 <bonoria63@gmail.com>
2020-03-25 01:28:14 -0400

    added mattock Tool Type

---
+++ src/main/java/net/MrBonono63/create/items/CreateAxeItem.java
@@ -0,0 +1,10 @@
+package net.MrBonono63.create.items;
+
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ToolMaterial;
+
+public class CreateAxeItem extends AxeItem {
+    public CreateAxeItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
